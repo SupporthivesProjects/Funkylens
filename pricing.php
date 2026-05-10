322a2aa5d272c8780e4435a12c7d22144b834596
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-05-10 09:49:47 +0530

    rahul

---
+++ pricing.php
@@ -0,0 +1,205 @@
+<?php include 'includes/header.php'; ?>
+<style>
+    header{
+        background: transparent;
+    }
+</style>
+<!-- Secion 1 Starts -->
+<section class="pricing_s1">
+    <div class="pricing_c1">
+        <h1 class="pricing_s1title">Pricing & Perks</h1>
+        <p class="pricing_s1subtitle">The more you buy, the more you save with our tiered pricing. Enter a specific amount below in credits or currency, and the other field will auto-update. Plus, enjoy our loyalty tiers: Bronze, Silver, and Gold, each offering more benefits. Maximize savings and get premium quality images with our credit packages.</p>
+    </div>
+</section>
+<!-- Secion 1 End -->
+
+<!-- Secion 2 Starts -->
+ <section class="pricing_s2">
+    <div class="pricing_c2">
+        <div class="pricing_c2inner">
+            <img class="hori_line1" src="./img/hori_line.svg">
+            <img class="hori_line2" src="./img/hori_line.svg">
+            <img class="ver_line1" src="./img/ver_line.svg">
+            <img class="ver_line2" src="./img/ver_line.svg">
+            <div class="pricing_s2titlebar">
+                <h1 class="pricing_s2title">Credit Packs</h1>
+                <p class="pricing_s2subtitle">Simple, transparent pricing that grows with you.<br><span class="pricing_s2subtitledark">1 standard image costs 1 credit.</span></p>
+            </div>
+            <div class="pricing_s2mid">
+                <div class="pricing_card active">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Starter</h2>
+                        <p class="pricing_cardcredittitle">35 credits, $30</p>
+                        <p class="pricing_cardsavetitle">Save 10%</p>
+                    </div>
+                    <button>Selected</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Basic<h2>
+                        <p class="pricing_cardcredittitle">60 credits, $50</p>
+                        <p class="pricing_cardsavetitle">Save 17%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Standard<h2>
+                        <p class="pricing_cardcredittitle">90 credits, $70</p>
+                        <p class="pricing_cardsavetitle">Save 22%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Advanced<h2>
+                        <p class="pricing_cardcredittitle">130 credits, $100</p>
+                        <p class="pricing_cardsavetitle">Save 23%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Pro<h2>
+                        <p class="pricing_cardcredittitle">220 credits, $150</p>
+                        <p class="pricing_cardsavetitle">Save 32%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Premium<h2>
+                        <p class="pricing_cardcredittitle">500 credits, $300</p>
+                        <p class="pricing_cardsavetitle">Save 40%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Business<h2>
+                        <p class="pricing_cardcredittitle">1,200 credits, $600</p>
+                        <p class="pricing_cardsavetitle">Save 50%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Enterprise<h2>
+                        <p class="pricing_cardcredittitle">3,500 credits, $1,500</p>
+                        <p class="pricing_cardsavetitle">Save 57%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Pro<h2>
+                        <p class="pricing_cardcredittitle">220 credits, $150</p>
+                        <p class="pricing_cardsavetitle">Save 32%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Premium<h2>
+                        <p class="pricing_cardcredittitle">500 credits, $300</p>
+                        <p class="pricing_cardsavetitle">Save 40%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Business<h2>
+                        <p class="pricing_cardcredittitle">1,200 credits, $600</p>
+                        <p class="pricing_cardsavetitle">Save 50%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+                <div class="pricing_card">
+                    <div class="pricing_cardcontentbar">
+                        <h2 class="pricing_cardtitle">Enterprise<h2>
+                        <p class="pricing_cardcredittitle">3,500 credits, $1,500</p>
+                        <p class="pricing_cardsavetitle">Save 57%</p>
+                    </div>
+                    <button>Select Package</button>
+                </div>
+            </div>
+            <button type="button" class="btn yellow_btnprice">Purchase Credits</button>
+        </div>
+    </div>
+    <div class="pricing_c3">
+        <div class="pricing_c3top">
+            <h1 class="pricing_s2title">Custom amount</h1>
+            <p class="pricing_s2subtitle">If you need a specific amount, please enter it below. You can input either the credit amount or the currency amount, and the other field will automatically update accordingly.<br><span class="pricing_c3subtitleyellow">1 standard image costs 1 credit.</span></p>
+        </div>
+        <div class="pricing_c3bottom">
+            <div class="pricing_textboxbar w-100">
+                <input type="text" class="form-control pricing_textbox w-100" id="" placeholder="Enter credit amount">
+                <input type="text" class="form-control pricing_textbox w-100" id="" placeholder="or currency amount">
+            </div>
+            <button type="button" class="btn yellow_btnprice w-100">Purchase X Credits</button>
+        </div>
+    </div>
+ </section>
+<!-- Secion 2 End -->
+
+<!-- Secion 3 Starts -->
+ <section class="pricing_s3">
+    <div class="pricing_c4 container">
+        <div class="pricing_s3top">
+            <h2 class="pricing_s3title">Rewarded for loyalty</h2>
+            <p class="pricing_s3subtitle">As you spend with ImageVault, you'll be rewarded with tier upgrades that provide even more benefits. Our loyalty program includes 3 tiers:</p>
+        </div>
+        <div class="pricing_s3mid">
+            <div class="pricing_s3midcard">
+                <img src="./img/bronze_loyalty.svg">
+                <div class="pricing_s3midcardbottom">
+                    <h3 class="pricing_s3midcardtitle">Bronze</h3>
+                    <p class="pricing_s3midcardsubtitle">Achieved after spending $300.</p>
+                    <p class="pricing_s3midcardsubtitlegreen">5% additional credits on each topup.</p>
+                </div>
+            </div>
+            <div class="pricing_s3midcard">
+                <img src="./img/silver_loyalty.svg">
+                <div class="pricing_s3midcardbottom">
+                    <h3 class="pricing_s3midcardtitle">Silver</h3>
+                    <p class="pricing_s3midcardsubtitle">Achieved after spending $600.</p>
+                    <p class="pricing_s3midcardsubtitlegreen">7% additional credits on each topup.</p>
+                </div>
+            </div>
+            <div class="pricing_s3midcard">
+                <img src="./img/gold_loyalty.svg">
+                <div class="pricing_s3midcardbottom">
+                    <h3 class="pricing_s3midcardtitle">Gold</h3>
+                    <p class="pricing_s3midcardsubtitle">Achieved after spending $1,500.</p>
+                    <p class="pricing_s3midcardsubtitlegreen">12% additional credits on each topup.</p>
+                </div>
+            </div>
+        </div>
+        <div class="pricing_s3bottom">
+            <p class="pricing_s3question">Still have questions?</p>
+            <button type="button" class="btn yellow_btnprice">View More FAQs</button>
+        </div>
+    </div>
+ </section>
+<!-- Secion 3 End -->
+<?php include 'includes/footer.php'; ?>
+<script>
+    const pricing_s2mid = document.querySelectorAll(".pricing_card");
+
+    pricing_s2mid.forEach(pricing_card => {
+
+    pricing_card.addEventListener("click", () => {
+
+        // remove active from all
+        pricing_s2mid.forEach(c => {
+        c.classList.remove("active");
+        c.querySelector("button").textContent = "Select Package";
+        });
+
+        // activate clicked pricing_card
+        pricing_card.classList.add("active");
+        pricing_card.querySelector("button").textContent = "Selected";
+    });
+
+    });
+</script>

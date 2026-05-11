9aec98e8bebdb4cb0e24ddb16e6dfa13576046e9
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-05-11 05:47:21 +0530

    rahul

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <?php include 'includes/header.php'; ?>
+<style>
+    header{
+        background: transparent;
+    }
+</style>
+<!-- Section 1 Start -->
+ <section class="home_s1">
 
-<section>
+ </section>
+<!-- Section 1 End -->
 
-</section>
-<section>
+<!-- Section 2 Start -->
+ <section></section>
+<!-- Section 2 End -->
 
-</section>
+<!-- Section 3 Start -->
+ <section></section>
+<!-- Section 3 End -->
 
+<!-- Section 4 Start -->
+ <section class="home_s4">
+    <div class="home_c4 container">
+        
+    </div>
+ </section>
+<!-- Section 4 End -->
+
+<!-- Section 5 Start -->
+ <section class="home_s5">
+    <div class="home_c5">
+        <h2 class="home_s5title">Our Story</h2>
+        <div class="home_s5subtitlebar">
+            <p class="home_s5subtitle">We are dedicated to providing high-quality, royalty-free stock images to elevate your creative projects. Our extensive library is curated by a community of talented contributors, ensuring a diverse selection of visuals for any need. We prioritize ease of use, affordability, and exceptional customer service, making ImageVault your trusted partner in visual content. Join us in transforming ideas into impactful visuals.</p>
+            <button type="button" class="btn yellow_btnprice">Read More</button>
+        </div>
+    </div>
+ </section>
+<!-- Section 5 End -->
 <?php include 'includes/footer.php'; ?>
